ec9ab31076455f7ca751f7858f018643d491e711
Ren Wang <iamwrm@gmail.com>
2023-03-26 02:36:35 +0000

    serialize works

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,31 @@ use serde_rusqlite::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ProcessRecord {
-    stat: procfs::process::Stat,
-    status: procfs::process::Status,
+    pid: i32,
+    name: String,
+    uid: u32,
+    vmrss: Option<u64>,
+}
+
+impl ProcessRecord {
+    fn try_new(
+        pid: i32,
+        stat: Option<procfs::process::Stat>,
+        status: Option<procfs::process::Status>,
+    ) -> Result<Self> {
+        if status.is_none() || stat.is_none() {
+            anyhow::bail!("status or stat is none")
+        }
+        let stat = stat.unwrap();
+        let status = status.unwrap();
+
+        Ok(Self {
+            pid,
+            name: status.name,
+            uid: status.egid,
+            vmrss: status.vmrss,
+        })
+    }
 }
 
 fn main() -> Result<()> {
@@ -17,16 +40,19 @@ fn main() -> Result<()> {
     let it = procfs::process::all_processes()?;
     let processes = it.map(|p| p.unwrap()).collect::<Vec<_>>();
 
-    println!("pids: {:?}", processes);
-
     let connection = rusqlite::Connection::open_in_memory()?;
 
     // get all processes's res memory usage
     for p in processes {
         let pid = p.pid;
-        let stat = p.stat().unwrap();
-        let status = p.status().unwrap();
-        println!("pid: {}, stat: {:?}, \nstatus: {:?} ", pid, stat, status);
+        let stat = p.stat().ok();
+        let status = p.status().ok();
+
+        if let Ok(record) = ProcessRecord::try_new(pid, stat, status) {
+            println!("record: {:?}", record);
+            // insert into sqlite
+            let parmas = to_params_named(&record)?;
+        }
     }
 
     Ok(())
