a4a0fa427d9c6658a652c1ebd5625f730066e3d1
Ren Wang <iamwrm@gmail.com>
2023-03-27 06:42:06 +0000

    check /app permission

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -25,7 +25,7 @@ docker run --rm \
 	-v ${HOME}/.cargo/registry/index:/home/u1000/.cargo/registry/index \
 	-v ${HOME}/.cargo/git/db:/home/u1000/.cargo/git/db \
 	rust_centos7_builder \
-		bash -c "sudo chmod -R 777 ~/.cargo && ls -lah ~/.cargo/registry && source ~/.cargo/env && cd /app && cargo build --release"
+		bash -c "sudo chmod -R 777 ~/.cargo && ls -lah ~/.cargo/registry && ls -lah /app && id && source ~/.cargo/env && cd /app && cargo build --release"
 
 docker build -t prs \
 	-f docker/Dockerfile.runtime .
