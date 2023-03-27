d483bb34588b13d9ea43e9a3613e2909714e3df7
Ren Wang <iamwrm@gmail.com>
2023-03-27 07:27:58 +0000

    try podman

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,22 +4,24 @@ set -ueo pipefail
 
 export DOCKER_BUILDKIT=1
 
+export DOCKER=podman
+
 # load 2 docker images if they exist
 
 if [ -f prs.tar ]; then
     echo "Loading prs.tar"
-    docker load -i prs.tar
+    ${DOCKER} load -i prs.tar
 fi
 
 if [ -f rust_centos7_builder.tar ]; then
     echo "Loading rust_centos7_builder.tar"
-    docker load -i rust_centos7_builder.tar
+    ${DOCKER} load -i rust_centos7_builder.tar
 fi
 
-docker build -t rust_centos7_builder \
+${DOCKER} build -t rust_centos7_builder \
 	-f docker/Dockerfile.builder . 
 
-docker run --rm \
+${DOCKER} run --rm \
 	-v ${PWD}:/app \
 	-v ${HOME}/.cargo/registry/cache:/home/u1000/.cargo/registry/cache \
 	-v ${HOME}/.cargo/registry/index:/home/u1000/.cargo/registry/index \
@@ -31,16 +33,19 @@ docker run --rm \
             && cd /app \
             && cargo build --release"
 
-docker build -t prs \
+${DOCKER} build -t prs \
 	-f docker/Dockerfile.runtime .
 
 find . -type d -print0 | xargs -0 chmod 0755 &
 find . -type f -print0 | xargs -0 chmod 0644 &
 
+wait
 # export 2 docker images 
 
-docker save -o prs.tar prs &
-docker save -o rust_centos7_builder.tar rust_centos7_builder &
+rm -f prs.tar rust_centos7_builder.tar
+
+${DOCKER} save -o prs.tar prs &
+${DOCKER} save -o rust_centos7_builder.tar rust_centos7_builder &
 
 wait
 
